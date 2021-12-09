337fcb86a854fca6798fcdeeb83f789bb14bcbff
Jorge Muñoz <jamunoz@Jorges-MacBook-Air.local>
2021-12-08 17:29:27 -0800

    Day 6 part 1

--- Day 6/main.go
+++ Day 6/main.go
@@ -1,2 +1,43 @@
 // Advent of Code -- Day 6: Go edition
+package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	datalist, _ := os.ReadFile("input.in")
+
+	// First parsing the string to get rid of commas
+	newList := strings.Split(string(datalist), ",")
+
+	// Converting that string array to int
+	var fishList = []int{}
+	for _, i := range newList {
+		num, _ := strconv.Atoi(i)
+		fishList = append(fishList, num)
+	}
+
+	// Find the number of lanternfish after 80 days
+	var days int = 80
+	var fishNum int = len(fishList)
+	for i := 0; i < days; i++ {
+		for fish, daysLeft := range fishList {
+			daysLeft--
+			// New fishy
+			if daysLeft < 0 {
+				daysLeft = 6
+				fishList = append(fishList, 8)
+				fishNum += 1
+			}
+			fishList[fish] = daysLeft
+		}
+	}
+
+
+	fmt.Println(fishNum)
+
+}
