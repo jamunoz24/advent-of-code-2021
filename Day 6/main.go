e9335baa81e719b0ba1c5fadf365727f693ada3b
Jorge Muñoz <jamunoz@Jorges-MacBook-Air.local>
2021-12-19 00:07:13 -0800

    Day 6 completed

--- Day 6/main.go
+++ Day 6/main.go
@@ -22,22 +22,70 @@ func main() {
 	}
 
 	// Find the number of lanternfish after 80 days
-	var days int = 80
-	var fishNum int = len(fishList)
-	for i := 0; i < days; i++ {
-		for fish, daysLeft := range fishList {
-			daysLeft--
-			// New fishy
-			if daysLeft < 0 {
-				daysLeft = 6
-				fishList = append(fishList, 8)
-				fishNum += 1
-			}
-			fishList[fish] = daysLeft
-		}
+	var days int = 256
+
+	fishCounts := make([]int, 9)
+
+	for _, num := range fishList {
+		fishCounts[num] += 1
 	}
 
+	for day := 0; day < days; day++ {
+		newFishies := fishCounts[0]
+		for i := 0; i < len(fishCounts)-1; i++ {
+			fishCounts[i] = fishCounts[i+1]
+		}
+		fishCounts[6] += newFishies
+		fishCounts[8] = newFishies
+	}
 
-	fmt.Println(fishNum)
+	// Getting sum
+	var sum uint = 0
+	for i := range fishCounts {
+		sum += uint(fishCounts[i])
+	}
+	fmt.Println(sum)
+
+	
+	// for _, fish := range fishList {
+	// 	daysLeft := days - fish
+	// 	childFishies := (daysLeft / 7) + 1
+	// 	fishNum += uint64(childFishies)
+
+	// 	// Populating the child fishy array
+	// 	var fishyArray = []int{}
+	// 	for daysLeft > -1 {
+	// 		fishyArray = append(fishyArray, daysLeft)
+	// 		daysLeft -= 7
+	// 	}
+
+	// 	// fmt.Println(fishyArray)
+
+	// 	for _, day := range fishyArray {
+	// 		countingEights(day, &fishNum)
+	// 	}
+	// }
+
+}
+
+
+func countingEights(days int, fishNum *uint64) {
+	daysLeft := days-9
+	if daysLeft < 0 {
+		return
+	}
+	childFishies := (daysLeft / 7) + 1
+	*fishNum += uint64(childFishies)
+
+	// Populating the child fishy array
+	var fishyArray = []int{}
+	for daysLeft > -1 {
+		fishyArray = append(fishyArray, daysLeft)
+		daysLeft -= 7
+	}
+
+	for _, day := range fishyArray {
+		countingEights(day, fishNum)
+	}
 
 }
